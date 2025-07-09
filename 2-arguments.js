8f03bda9b155c4af630c8298ed32874c28e02859
Moses Rametsi <mosesrametsi16@gmail.com>
2025-07-09 17:20:45 +0200

    Add js file: 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,17 @@
+function name(nickname) {
+    console.log(nickname, ":No argument");
+}
+
+name();
+
+function name1(nickname) {
+     console.log(nickname, ":Argument found");
+}
+
+name1("Goku");
+
+function name2(nickname, nickname2) {
+     console.log(nickname, nickname2, ":Arguments found");
+}
+
+name2("Goku", "Vegeta");
