e36305d3e31f62b803e1097e3ffa0e56d6722e0d
Moses Rametsi <mosesrametsi16@gmail.com>
2025-07-09 17:25:20 +0200

    Fix formating

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,17 +1,17 @@
 function name(nickname) {
-    console.log(nickname, ":No argument");
+    console.log("No argument");
 }
 
 name();
 
 function name1(nickname) {
-     console.log(nickname, ":Argument found");
+     console.log("Argument found");
 }
 
 name1("Goku");
 
 function name2(nickname, nickname2) {
-     console.log(nickname, nickname2, ":Arguments found");
+     console.log("Arguments found");
 }
 
 name2("Goku", "Vegeta");
