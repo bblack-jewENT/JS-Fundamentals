396c024160008a7a425aea5a0a8303cb152dd590
Moses Rametsi <mosesrametsi16@gmail.com>
2025-07-09 16:56:02 +0200

    Fix output error.

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,4 @@
-let message = ["C is fun", "Python is fun", "JavaScript is amazing"];
+const message = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 for (let i = 0; i < message.length; i++) {
     console.log(message[i]);
