2b33ef16bf8d170b2be112f52fc11f0e2731d172
Moses Rametsi <mosesrametsi16@gmail.com>
2025-07-10 18:17:18 +0200

    Add multiple function declaration.

--- 4-concat.js
+++ 4-concat.js
@@ -5,10 +5,16 @@ function args(arg1, arg2) {
     console.log(`${arg1} is ${arg2}`);
 }
 
-if (arg1 === process.argv[2]) {
-    console.log(args("Python", "fun"));
-} else if (arg2 === process.argv[3]) {
-    console.log(args("HBTN"));
-} else {
-    console.log(args());
+args("Python", "fun");
+
+function args1(arg1, arg2) {
+    console.log(`${arg1} is ${arg2}`);
 }
+
+args1("HBTN");
+
+function args2(arg1, arg2) {
+    console.log(`${arg1} is ${arg2}`);
+}
+
+args2();
