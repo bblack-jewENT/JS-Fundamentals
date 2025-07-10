8981252719643c94e253c1b300b95b107f873e8a
Moses Rametsi <mosesrametsi16@gmail.com>
2025-07-11 00:06:59 +0200

    Remove const variables.

--- 4-concat.js
+++ 4-concat.js
@@ -1,10 +1,9 @@
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+
 
 function args(arg1, arg2) {
     console.log(`${arg1} is ${arg2}`);
 }
 
 args("Python", "fun");
-args("HBTN", "");
-args("", "");
+args("HBTN");
+args();
