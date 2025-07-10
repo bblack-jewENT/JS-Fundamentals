9161044d4866f880e5deff9a98f7e27d890cd6e1
Moses Rametsi <mosesrametsi16@gmail.com>
2025-07-11 00:26:22 +0200

    Delete function

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,4 @@
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
-
-function args(arg1, arg2) {
-    console.log(`${arg1} is ${arg2}`);
-}
-
-args("Python", "fun");
-args("HBTN");
-args();
+console.log(`${arg1} is ${arg2}`);
