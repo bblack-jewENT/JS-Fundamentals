f25a3d3093d1df988734f95b5d325de2cb7dc0d1
Moses Rametsi <mosesrametsi16@gmail.com>
2025-07-12 03:19:18 +0200

    Delete else if statement.

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,9 +1,7 @@
 const number = process.argv[2];
 
-if (number === 'school') {
+if (number === String || number === undefined) {
     console.log("Not a number");
-} else if (number === undefined) {
-   console.log("Not a number")
 } else {
      console.log(`My number: ${number}`);
 }
